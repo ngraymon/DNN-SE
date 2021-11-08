904f653428b2adf757f676b23b74005944232eef
AJFrancey <Andrew@torbellon.com>
2021-11-08 17:34:10 -0500

    Finished the first functions psuodo code.

--- hamiltonian.py
+++ hamiltonian.py
@@ -19,13 +19,13 @@ def kinetic_from_log(f,x):
     
 def operators(atoms, nelectrons, potential_epsilon=0.0):
     
-    POTENTIAL = 0.0   
+    Nucleus to Nucleus Potential = 0.0   
     
     FOR EACH ATOM I IN THE SYSTEM:
         FOR each atom in the system after atom I:
             CHARGE = ATOM1 CHARGE * ATOM2 CHARGE
             POTENTIAL = CHARGE/(DISTANCE BETWEEN ATOMS)
-            
+            ADD potential to Nucleus to Nuclesu Potential
             
     def smooth_norm(x):
         
@@ -37,11 +37,33 @@ def operators(atoms, nelectrons, potential_epsilon=0.0):
     
     def nuclear_potential(xs):
         
-        CREATE an empty list for potential
+        CREATE an empty list for electron nucleus potential 
         
         FOR atom in atoms:
             ADD (-charge/ smooth_norm(coord - x) for x in sx) to the potential list 
         RETURN the sum of the potential lists for all the atom in atoms
     
     
+    def electronic_potential(xs):
         
+        IF there is more then one electron
+            CREATE an empty list for electron electron potential
+            For every electron in the system
+                APPEND the inverse of the smooth_norm of the distance between this electron and all the other electrons
+            RETURN the sum of the electron electron potentials
+        ELSE 
+            RETURN 0.0
+    
+    
+    def nuclear_nuclear(dtype):
+        RETURN the Nucleus to Nucleus potential 
+        
+    
+    def potential(x):
+        SPLIT x into nelectrons arrays
+        
+        RETURN the sum of the nuclear_potential, the electronic_potential, and the nuclear_nuclear
+    
+    RETURN the kinetic_from_log, potential
+    
+
