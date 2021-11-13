738602a11ef46cde21ba6ec6b11aa43905178b6a
Neil <ngraymon@uwaterloo.ca>
2021-11-13 17:54:18 -0500

    Remove extraneous files

--- testing.py
+++
@@ -1,9 +0,0 @@
-import torch
-import numpy as np
-
-from FNN import *
-
-model = FermiNet(5, 1, np.array([[1,1,1], [-1,1,1]]), np.array([[0,2,1], [0,0,1]]), num_determinants=2)
-
-wavefn = model.forward()
-print(wavefn)
