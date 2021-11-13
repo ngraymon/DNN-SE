0109b88b22d5a615cd26a784887e38a1a3c83f70
Neil <ngraymon@uwaterloo.ca>
2021-11-13 18:15:28 -0500

    Readability changes
    my personal preference;
    feel free to challenge any changes you disagree with

--- fnn.py
+++ fnn.py
@@ -5,6 +5,7 @@ This module defines the neural network.
 """
 
 # system imports
+import itertools as it
 
 # third party imports
 import torch
@@ -24,10 +25,14 @@ class FermiNet(torch.nn.Module):
         self.n_up = n_up
         self.num_determinants = num_determinants
 
-        # default configuration, ensures correct dimensions in first layer, and keeps all h vectors the same length
+        """ default configuration, ensures correct dimensions in first layer, and keeps all h vectors the same length
+
+        the final double stream layer is not necessary
+        so setting `custom_h_sizes[-1]` to zero gives empty weight matrices and saves computations
+        """
         if custom_h_sizes is False:
             custom_h_sizes = [[4*I, 4] for i in range(L+1)]
-            custom_h_sizes[-1][1] = 0   # final double stream layer not necessary, so setting to zero gives empty weight matrices and saves computations
+            custom_h_sizes[-1][1] = 0  # layers that are not connected have a weight of zero
 
         self.layers = [FermiLayer(n, custom_h_sizes[i], custom_h_sizes[i+1]) for i in range(L)]
 
@@ -37,19 +42,31 @@ class FermiNet(torch.nn.Module):
         # single stream inputs:
         eN_vectors = np.array([[i-j for j in nuclei_positions] for i in electron_positions])
         self.eN_vectors = torch.from_numpy(eN_vectors)
+
         self.inputs[0] = torch.from_numpy(np.array([
             np.concatenate([
-                np.concatenate([eN_vectors[i][j] for j in range(I)], axis=None),
+                np.concatenate(
+                    [eN_vectors[i][j] for j in range(I)],
+                    axis=None
+                ),
                 [np.linalg.norm(eN_vectors[i][j]) for j in range(I)]
-            ]) for i in range(n)]))
+            ])
+            for i in range(n)
+        ]))
 
         # double stream inputs:
         ee_vectors = [[i-j for j in electron_positions] for i in electron_positions]
-        self.inputs[1] = torch.from_numpy(np.array([[
-            np.concatenate([
-                ee_vectors[i][j],
-                [np.linalg.norm(ee_vectors[i][j])]
-            ]) for j in range(n)] for i in range(n)]))
+
+        self.inputs[1] = torch.from_numpy(np.array([
+            [
+                np.concatenate([
+                    ee_vectors[i][j],
+                    [np.linalg.norm(ee_vectors[i][j])]
+                ])
+                for j in range(n)
+            ]
+            for i in range(n)
+        ]))
 
         # Randomly initialise trainable parameters:
         self.final_weights = torch.nn.Parameter(torch.rand(self.num_determinants, n, custom_h_sizes[-1][0]))# w vectors
@@ -74,18 +91,16 @@ class FermiNet(torch.nn.Module):
         for k in range(self.num_determinants):
 
             # up spin:
-            for i in range(self.n_up):
-                for j in range(self.n_up):
-                    final_dot = torch.dot(self.final_weights[k][i], layer_outputs[-1][0][j]) + self.final_biases[k][i]
-                    env_sum = torch.sum(torch.stack([self.pi_weights[k][i][m]*torch.exp(-torch.norm(self.sigma_weights[k][i][m] * self.eN_vectors[j][m])) for m in range(self.I)]))
-                    phi_up[k][i][j] = final_dot * env_sum
+            for i, j in it.product(range(self.n_up), repeat=2):
+                final_dot = torch.dot(self.final_weights[k][i], layer_outputs[-1][0][j]) + self.final_biases[k][i]
+                env_sum = torch.sum(torch.stack([self.pi_weights[k][i][m]*torch.exp(-torch.norm(self.sigma_weights[k][i][m] * self.eN_vectors[j][m])) for m in range(self.I)]))
+                phi_up[k][i][j] = final_dot * env_sum
 
             # down spin:
-            for i in range(self.n_up, self.n):
-                for j in range(self.n_up, self.n):
-                    final_dot = torch.dot(self.final_weights[k][i], layer_outputs[-1][0][j]) + self.final_biases[k][i]
-                    env_sum = torch.sum(torch.stack([self.pi_weights[k][i][m]*torch.exp(-torch.norm(self.sigma_weights[k][i][m] * self.eN_vectors[j][m])) for m in range(self.I)]))
-                    phi_down[k][i-self.n_up][j-self.n_up] = final_dot * env_sum
+            for i, j in it.product(range(self.n_up, self.n), repeat=2):
+                final_dot = torch.dot(self.final_weights[k][i], layer_outputs[-1][0][j]) + self.final_biases[k][i]
+                env_sum = torch.sum(torch.stack([self.pi_weights[k][i][m]*torch.exp(-torch.norm(self.sigma_weights[k][i][m] * self.eN_vectors[j][m])) for m in range(self.I)]))
+                phi_down[k][i-self.n_up][j-self.n_up] = final_dot * env_sum
 
         # Compute determinants:
         d_up = torch.det(phi_up)
@@ -105,22 +120,32 @@ class FermiLayer(torch.nn.Module):
         super().__init__()
 
         f_vector_length = 3*h_in_dims[0] + 2*h_in_dims[1]
-        # matrix and bias vector for each single stream's linear op, applied to the f vector and yielding a vector of the output length
-        self.v_matrices = torch.nn.Parameter(torch.rand(n, f_vector_length, h_out_dims[0]))  # vector of matrices
-        self.b_vectors = torch.nn.Parameter(torch.rand(n, h_out_dims[0]))  # vector of vectors
-        # matrix and bias vector for each double stream's linear op
-        self.w_matrices = torch.nn.Parameter(torch.rand(n, n, h_in_dims[1], h_out_dims[1]))  # matrix of matrices
-        self.c_vectors = torch.nn.Parameter(torch.rand(n, n, h_out_dims[1]))  # matrix of vectors
+
+        """ matrix and bias vector for each single stream's linear op applied to the f vector
+            and yielding a vector of the output length
+
+        self.v_matrices is a vector of matrices
+        self.b_vectors is a vector of vectors
+        """
+        self.v_matrices = torch.nn.Parameter(torch.rand(n, f_vector_length, h_out_dims[0]))
+        self.b_vectors = torch.nn.Parameter(torch.rand(n, h_out_dims[0]))
+
+        """ matrix and bias vector for each double stream's linear op applied to the f vector
+            and yielding a vector of the output length
+
+        self.w_matrices is a matrix of matrices
+        self.c_vectors is a matrix of vectors
+        """
+        self.w_matrices = torch.nn.Parameter(torch.rand(n, n, h_in_dims[1], h_out_dims[1]))
+        self.c_vectors = torch.nn.Parameter(torch.rand(n, n, h_out_dims[1]))
 
     def forward(self, input_tensor, n_up):
         """ x """
 
         # single layers:
         single_h, double_h = input_tensor[0].type(torch.FloatTensor), input_tensor[1].type(torch.FloatTensor)
-        single_h_up = single_h[:n_up]
-        single_h_down = single_h[n_up:]
-        double_h_ups = double_h[:, :n_up]
-        double_h_downs = double_h[:, n_up:]
+        single_h_up, single_h_down = single_h[:n_up], single_h[n_up:]
+        double_h_ups, double_h_downs = double_h[:, :n_up], double_h[:, n_up:]
 
         single_g_up = torch.mean(single_h_up, 0)
         single_g_down = torch.mean(single_h_down, 0)
@@ -128,6 +153,7 @@ class FermiLayer(torch.nn.Module):
         double_g_downs = torch.mean(double_h_downs, 1)  # Note: double check which axis?
 
         n = len(input_tensor[0])
+
         f_vectors = torch.stack([
             torch.cat((
                 single_h[i],
@@ -141,7 +167,18 @@ class FermiLayer(torch.nn.Module):
         print(torch.transpose(self.v_matrices, 1, 2).size(), f_vectors[:, :, None].size())
         print(self.v_matrices.type(), f_vectors.type())
         # single_output = torch.tanh(torch.bmm(torch.transpose(self.v_matrices, 1, 2), f_vectors[:,:,None]) + self.b_vectors)  # Note: check dimensions order for torch.mul are correct??
-        single_output = torch.tanh(torch.squeeze(torch.matmul(torch.transpose(self.v_matrices, 1, 2), f_vectors[:, :, None]), 2) + self.b_vectors)  # Note: check dimensions order for torch.mul are correct??
+
+        single_output = torch.tanh(
+            torch.squeeze(
+                # Note: check dimensions order for torch.mul are correct??
+                torch.matmul(
+                    torch.transpose(self.v_matrices, 1, 2),
+                    f_vectors[:, :, None]
+                ),
+                dim=2
+            ) + self.b_vectors
+        )
+
         print(single_output.size())
         # output[0] = np.tanh(torch.tensor([(self.v_matrices[i] @ f_vectors[i]) + self.b_vectors[i] for i in range(len(f_vectors))]))
         if single_output.size() == single_h.size():
@@ -151,7 +188,17 @@ class FermiLayer(torch.nn.Module):
         # double_output = torch.tanh(torch.mul(self.w_matrices, double_h) + self.c_vectors)#Note: check dimensions order for @ (np.matmul) are correct??
         print(torch.flatten(self.w_matrices, end_dim=1).size(), torch.flatten(double_h[:, :, :, None], end_dim=1).size())
         print(self.c_vectors.size())
-        double_output = torch.tanh(torch.squeeze(torch.bmm(torch.flatten(self.w_matrices, end_dim=1), torch.flatten(double_h[:, :, :, None], end_dim=1)), 2) + torch.flatten(self.c_vectors, end_dim=1))  # Note: check dimensions order for torch.mul are correct??
+
+        # Note: check dimensions order for torch.mul are correct??
+        double_output = torch.tanh(
+            torch.squeeze(
+                torch.bmm(
+                    torch.flatten(self.w_matrices, end_dim=1),
+                    torch.flatten(double_h[:, :, :, None], end_dim=1)
+                ),
+                dim=2
+            ) + torch.flatten(self.c_vectors, end_dim=1)
+        )
 
         # reshape:
         shape = list(double_output.shape)[1:]
