c9cc959662a0ecc95a3936b7cda7171784f586d9
Neil <ngraymon@uwaterloo.ca>
2021-11-13 17:52:55 -0500

    cleanup - flake8/syntax improvements

--- train.py
+++ train.py
@@ -1,12 +1,23 @@
+""" train module
+
+This module preforms the training
+
+"""
+
+# system imports
+
+# third party imports
 import torch
 from torch import optim
 from torch.autograd import grad_mode
 from torch.optim import optimizer
 from torch.optim.optimizer import Optimizer
 
+# local imports
+
 
 class Train():
-    def __init__(self,network,mcmc,Hamiltonian,param):
+    def __init__(self, network, mcmc, Hamiltonian, param):
         """
         network: a call to the ferminet network class
 
@@ -17,52 +28,51 @@ class Train():
 
         param: hyperparameters
         """
-        self.net=network
-        self.mcmc=mcmc
-        self.H=Hamiltonian
-        self.param=param
-        self.optimizer=optim.Adam(self.net.parameters(),param['lr'])
-
+        self.net = network
+        self.mcmc = mcmc
+        self.H = Hamiltonian
+        self.param = param
+        self.optimizer = optim.Adam(
+            self.net.parameters(),
+            param['lr']
+        )
 
     def train_KFAC(self):
         return 0
-
-
-
 
     def train(self, bool_KFAC=False):
         if bool_KFAC:
             self.train_KFAC()
 
-        losstot=[]
-        phi_phisgn=[[]]
+        losstot = []
+        phi_phisgn = [[]]
         for i in range(self.param['epoch']):
-        #creating the walkers...
-            walkers=self.mcmc.create() #creates the configurations for each electron
-            #for a given batch size
-            self.net.zero_grad()
-            #get wavefunction for each one of these configuration
-            phi, phi_sgn=self.net.forward(walkers)
 
-            #from the hamiltonian extract potential and kinetic energy
-            kinetic=self.H.kinetic(phi, walkers).detach()
+            # creating the walkers...
+            walkers = self.mcmc.create()  # creates the configurations for each electron
+
+            # for a given batch size
+            self.net.zero_grad()
+
+            # get wavefunction for each one of these configuration
+            phi, phi_sgn = self.net.forward(walkers)
+
+            # from the hamiltonian extract potential and kinetic energy
+            kinetic = self.H.kinetic(phi, walkers).detach()
             potential = self.H.potential(walkers).detach()
             local_energy = kinetic + potential
 
-            # this is the "real" loss of the system
-            loss=torch.mean(local_energy)
+            #  this is the "real" loss of the system
+            loss = torch.mean(local_energy)
 
-            #here is the loss being passed into the backward pass since we have an explicit
-            #expression for the gradient of the loss
-            computedloss=torch.mean((local_energy-loss)*phi)
+            # here is the loss being passed into the backward pass since we have an explicit
+            # expression for the gradient of the loss
+            computedloss = torch.mean((local_energy-loss)*phi)
 
-            #compute the gradient wrt to weights and update with ADAM
+            # compute the gradient wrt to weights and update with ADAM
             (computedloss).backward()
             Optimizer.step()
             losstot.append(loss)
-            #phi_phisgn.append()
+            # phi_phisgn.append()
+
         return losstot
-
-
-
-
