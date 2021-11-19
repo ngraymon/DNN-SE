81d2a33eca34b25d41626fc5a7c37cd86902a52d
jbvalentin2001 <90353181+jbvalentin2001@users.noreply.github.com>
2021-11-18 19:13:23 -0500

    change the phi update within the MCMC

--- train.py
+++ train.py
@@ -32,10 +32,7 @@ class Train():
         self.mcmc = mcmc
         self.H = Hamiltonian
         self.param = param
-        self.optimizer = optim.Adam(
-            self.net.parameters(),
-            param['lr']
-        )
+        self.optimizer = optim.Adam(self.net.parameters(), param['lr'])
 
     def train_KFAC(self):
         return 0
@@ -44,25 +41,23 @@ class Train():
         if bool_KFAC:
             self.train_KFAC()
 
+        phi, walkers = self.mcmc.init()
+
         losstot = []
         phi_phisgn = [[]]
         for i in range(self.param['epoch']):
-
             # creating the walkers...
-            walkers = self.mcmc.create()  # creates the configurations for each electron
-
-            # for a given batch size
             self.net.zero_grad()
-
-            # get wavefunction for each one of these configuration
-            phi, phi_sgn = self.net.forward(walkers)
+            # get wavefunction for each one of these configuration, creates the configurations for each electron
+            # for a given batch size
+            phi, walkers, accuracy = self.mcmc.update()
 
             # from the hamiltonian extract potential and kinetic energy
             kinetic = self.H.kinetic(phi, walkers).detach()
             potential = self.H.potential(walkers).detach()
             local_energy = kinetic + potential
 
-            #  this is the "real" loss of the system
+            # this is the "real" loss of the system
             loss = torch.mean(local_energy)
 
             # here is the loss being passed into the backward pass since we have an explicit
@@ -70,9 +65,8 @@ class Train():
             computedloss = torch.mean((local_energy-loss)*phi)
 
             # compute the gradient wrt to weights and update with ADAM
-            (computedloss).backward()
+            computedloss.backward()
             Optimizer.step()
             losstot.append(loss)
             # phi_phisgn.append()
-
         return losstot
