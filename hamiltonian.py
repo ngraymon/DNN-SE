Attempted Fix - tried hessian, doesn't fix
we still get nans

hamiltonian.py
@@ -14,12 +14,13 @@ Import this module as:
 import numpy as np
 import torch
 from torch.autograd import grad, backward
+from torch.autograd.functional import hessian
 
 # local imports
 from log_conf import log
 
 
-def kinetic_from_log(f,NN ,x):
+def kinetic_from_log(f, x, network, using_hessian=False):
     '''
     Computes the kinetic energy from the log of |psi|,
     the -1/2 \nabla^2 \\psi / \\psi.
@@ -65,49 +66,63 @@ def kinetic_from_log(f,NN ,x):
 
     import pdb; pdb.set_trace()
 
-    # log.debug(f"\n{df = }")
-    # df[0] = df[1]
-    # log.debug(f"\n{df = }")
-    # import pdb; pdb.set_trace()
+    if not using_hessian:
+        # log.debug(f"\n{df = }")
+        # df[0] = df[1]
+        # log.debug(f"\n{df = }")
+        # import pdb; pdb.set_trace()
 
-    # loop over each psi_i
-    # sized (10, 3) we pick (10, 1) and broadcast the grad of that with x (10, 3)
-    #
-    # for i in range(x.shape[1]):
+        # loop over each psi_i
+        # sized (10, 3) we pick (10, 1) and broadcast the grad of that with x (10, 3)
+        for i in range(x.shape[1]):
 
-    #     # log.debug(f"{x.shape = }")
-    #     # log.debug(f"{df[..., i].shape = }")
-    #     # log.debug(f"{x = }")
-    #     # log.debug(f"{df[..., i] = }")
-    #     # log.debug(f"{torch.unsqueeze(df[..., i], -1).shape = }")
-    #     # import pdb; pdb.set_trace()
+            # log.debug(f"{x.shape = }")
+            # log.debug(f"{df[..., i].shape = }")
+            # log.debug(f"{x = }")
+            # log.debug(f"{df[..., i] = }")
+            # log.debug(f"{torch.unsqueeze(df[..., i], -1).shape = }")
+            # import pdb; pdb.set_trace()
 
-    #     # detached = df.detach()
-    #     input_df = torch.unsqueeze(df[..., i], -1)
-    #     log.debug(f"\n{input_df = }")
-    #     log.debug(f"\n{x = }")
-    #     import pdb; pdb.set_trace()
-    #     df2 = grad(
-    #         input_df,
-    #         x,
-    #         # allow_unused=True,
-    #         # create_graph=True,
-    #         retain_graph=True,
-    #         grad_outputs=torch.ones_like(input_df)
-    #     )
-    #     log.debug(f"\n{df2 = }")
-    #     # log.debug(f"{ = }")
-    #     # assert (2 == df2).all()
-    #     log.debug(f"{df2.shape = }")
+            # detached = df.detach()
+            input_df = torch.unsqueeze(df[..., i], -1)
+            log.debug(f"\n{input_df = }")
+            log.debug(f"\n{x = }")
 
-    #     lapl_elem = df2[..., i]
-    #     log.debug(f"{lapl_elem.shape = }")
-    #     lapl_tensor.append(lapl_elem)
+            import pdb; pdb.set_trace()
 
-    # log.debug(f"{lapl_tensor = }")
-    # import pdb; pdb.set_trace()
-    # lapl_tensor = torch.tensor(lapl_tensor)
-    lapl_tensor=torch.diagonal(torch.autograd.functional.hessian(NN.forward(),x))
+            df2, = grad(
+                input_df,
+                x,
+                # allow_unused=True,
+                # create_graph=True,
+                retain_graph=True,
+                grad_outputs=torch.ones_like(input_df)
+            )
+            log.debug(f"\n{df2 = }")
+            # log.debug(f"{ = }")
+            # assert (2 == df2).all()
+            log.debug(f"{df2.shape = }")
+
+            lapl_elem = df2[..., i]
+            log.debug(f"{lapl_elem.shape = }")
+            lapl_tensor.append(lapl_elem)
+
+        log.debug(f"{lapl_tensor = }")
+        import pdb; pdb.set_trace()
+        lapl_tensor = torch.tensor(lapl_tensor)
+
+    # an attempt at using hessian
+    elif using_hessian:
+
+        print(x)
+        print(torch.sum(x**2))
+        hess, = hessian(
+            lambda x: torch.sum(x**2),
+            x,
+            create_graph=True,
+        )
+        lapl_tensor = torch.diagonal(hess)
+
     lapl = torch.sum(lapl_tensor, axis=0) + torch.sum(df**2, axis=-1)
 
     return -0.5*torch.unsqueeze(lapl, -1)

train.py
@@ -52,34 +52,45 @@ class Train():
             # get wavefunction for each one of these configuration, creates the configurations for each electron
             # for a given batch size
             phi, walkers, accuracy = self.mcmc.preform_one_step()
-            phi_safe=torch.tensor(phi) #make sure the grad of phi is not changed in 
+
+            copy_of_phi = torch.tensor(phi)  # make sure the grad of phi is not changed in `self.kinetic`
+
             assert not torch.any(torch.isnan(walkers)), 'state configuration is borked'
 
             # from the Hamiltonian extract potential and kinetic energy
-            kinetic = self.kinetic(phi, walkers).detach()
+            kinetic = self.kinetic(phi, walkers, self.net).detach()
             potential = self.potential(walkers).detach()
             local_energy = kinetic + potential
 
             # this is the "real" loss of the system, i.e the mean of the loss for that batch size
             loss = torch.mean(local_energy)
 
-            if self.clip_el is not None:
+            # default for now
+            if self.clip_el is None:
+                # here is the loss being passed into the backward pass since we have an explicit
+                # expression for the gradient of the loss
+                computed_loss = torch.mean((local_energy - loss) * copy_of_phi)
+
+            else:
+
+                #
                 median = torch.median(local_energy)
-                diff = torch.mean(torch.abs(local_energy-median))
-                clipedlocal = torch.clip(
+
+                #
+                diff = torch.mean(torch.abs(local_energy - median))
+
+                #
+                cliped_local = torch.clip(
                     local_energy,
                     median - self.clip_el*diff,
                     median + self.clip_el*diff
                 )
-                computedloss = torch.mean((local_energy-loss)*phi_safe)
 
-            else:
-                # here is the loss being passed into the backward pass since we have an explicit
-                # expression for the gradient of the loss
-                computedloss = torch.mean((local_energy-loss)*phi_safe)
+                #
+                computed_loss = torch.mean((cliped_local - loss) * copy_of_phi)
 
             # compute the gradient w.r.t. the weights and update with ADAM
-            computedloss.backward()
+            computed_loss.backward()
             Optimizer.step()
             losstot.append(loss)
             # phi_phisgn.append()