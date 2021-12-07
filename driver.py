Fix - the whole network broadcasts over MC replicas
for some reason the 2nd derivative is still a bust even if I 'cheat'
and normalize the wavefunction

for more information about optimal dot products/ matmuls see
https://discuss.pytorch.org/t/dot-product-batch-wise/9746/12
https://pytorch.org/docs/stable/generated/torch.mul.html
https://github.com/pytorch/pytorch/issues/18027

specifically the github issue shows that torch.mul().sum() is way more
efficient

driver.py
@@ -393,7 +393,8 @@ def generate_electron_position_vector(molecule, electron_spec):
     log.debug(f"\nwalker tensor:\n{tab}{walker_string}")
 
     # glue the two lists together
-    electron_positions = np.concatenate(up_spin_list + down_spin_list)
+    # print(proposed_electron_spec)
+    electron_positions = np.array(both_spins_list).reshape((sum(electron_spec), 3))
 
     return electron_positions
 
@@ -607,7 +608,7 @@ def prepare_network(molecule, nof_electrons, spin_config):
         electron_positions,
         nuclear_positions,
         flags.hidden_units,
-        num_determinants=2
+        num_determinants=flags.determinants
     )
 
 

hamiltonian.py
@@ -37,30 +37,40 @@ def kinetic_from_log(f, x, network, using_hessian=False):
     The kinetic energy function.
     '''
 
+    # cheat and normalize f
+    log.debug(f"before normalization \n{f = }")
+    print(max(f.clone()))
+    print(f / max(f.clone()))
+    f /= min(f.clone())
+    log.debug(f"after normalization  \n{f = }")
+    import pdb; pdb.set_trace()
+
     n_replicas = int(x.shape[0])
 
     log.debug(f"{n_replicas = }")
-    # log.debug(f"{f = }")
-    # log.debug(f"{x = }")
+    # log.debug(f"\n{f = }")
+    # log.debug(f"\n{x = }")
+    log.debug(f"{f.shape = }")
+    log.debug(f"{x.shape = }")
 
     lapl_tensor = []
-    import pdb; pdb.set_trace()
 
     # do the fake x^2 thing
-    if True:
+    if False:
         y = x**2
         df, = grad(y, x, create_graph=True, grad_outputs=torch.ones_like(f))
     # what we actually want to do
     else:
-        df, = grad(f, x, create_graph=True, grad_outputs=torch.ones_like(f))
+        df, = grad(f, x, create_graph=True, allow_unused=True, grad_outputs=torch.ones_like(f))
+        # df, = grad(f, x, create_graph=True, grad_outputs=torch.ones_like(f))
 
     # # assert (df == x*2).all(), 'not the same'
 
-    log.debug(f"\n{f = }")
-    log.debug(f"\n{df = }")
-    log.debug(f"\n{2*x = }")
+    # log.debug(f"\n{f = }")
+    # log.debug(f"\n{df = }")
     # log.debug(f"\n{df[..., 0] = }")
     # log.debug(f"\n{x = }")
+    log.debug(f"{f.shape = }")
     log.debug(f"{df.shape = }")
     log.debug(f"{x.shape = }")
 
@@ -85,8 +95,10 @@ def kinetic_from_log(f, x, network, using_hessian=False):
 
             # detached = df.detach()
             input_df = torch.unsqueeze(df[..., i], -1)
-            log.debug(f"\n{input_df = }")
-            log.debug(f"\n{x = }")
+            log.debug(f"\n{input_df[0] = }")
+            log.debug(f"\n{x[0] = }")
+            log.debug(f"{x.shape = }")
+            log.debug(f"{input_df.shape = }")
 
             import pdb; pdb.set_trace()
 
@@ -98,9 +110,7 @@ def kinetic_from_log(f, x, network, using_hessian=False):
                 retain_graph=True,
                 grad_outputs=torch.ones_like(input_df)
             )
-            log.debug(f"\n{df2 = }")
-            # log.debug(f"{ = }")
-            # assert (2 == df2).all()
+            log.debug(f"\n{df2[0] = }")
             log.debug(f"{df2.shape = }")
 
             lapl_elem = df2[..., i]

monte_carlo.py
@@ -101,31 +101,35 @@ class MonteCarlo():
         # when sampling
         # self.walker_shape = (batch_size, len(initial_offset))
         self.batch_size = batch_size
-        # self.walkers = np.zeros(shape=self.walker_shape, dtype=dtype)
 
         number_of_replicas = 1  # how many GPUs we are using
 
         shape = (number_of_replicas, batch_size)
         self.walkers = self._initial_random_states(shape)
-        self.net_multi = bool(len(self.walkers.shape) >= 3)
 
         # number of monte carlo steps to preform per epoch/network call
         self.nof_steps = nof_steps
 
         # how we analyze our mc progress
-        log.debug(f"{self.walkers.shape = }")
         self.psi = self.compute_psi(self.walkers)
+        log.debug(f"{self.walkers.shape = }")
         log.debug(f"{self.psi.shape = }")
+        log.debug(f"{self.psi = }")
 
-        assert not torch.isnan(self.psi), 'Initial wavefunction is nan'
+        assert not torch.isnan(self.psi).any(), f"Initial wavefunction contains nan's:\n{self.psi = }"
         self.rolling_accuracy = 0.0
 
         return
 
     def compute_psi(self, visible_nodes, *args, **kwargs):
         """ x """
-        kwargs.update({'multi': self.net_multi})
-        return self.net.forward(visible_nodes, *args, **kwargs)
+        ret = self.net.forward(visible_nodes, *args, **kwargs)
+        assert ret.requires_grad is True
+        print('a', ret, ret.shape)
+        # ret /= max(ret.clone())
+        # print(ret)
+        import pdb; pdb.set_trace()
+        return ret
 
     def pre_train(HF_orbitals, nof_steps, **kwargs):
         """
@@ -183,8 +187,10 @@ class MonteCarlo():
         """
 
         # generate our uniform random number
-        u = torch.rand(size=self.walkers.shape)
-        log.debug(f"{'uniform random number at index 0':<30}{u[0, ...]}")
+        u = torch.rand(size=self.walkers.shape[:1])
+        log.debug(f"{u.shape = }")
+        log.debug(f"{acceptance_ratio.shape = }")
+        log.debug(f"{'uniform random number':<30}{u}")
 
         # test the condition
         accepted = u <= acceptance_ratio
@@ -214,7 +220,7 @@ class MonteCarlo():
         new_state = self.propose_new_state()
         # here ferminet seems to take the zeroth element of the array
         new_psi = self.compute_psi(new_state)
-        assert not torch.isnan(new_psi), 'New psi is nan'
+        assert not torch.isnan(new_psi).any(), f"New wavefunction contains nan's:\n{new_psi = }"
 
         # 3 - compute acceptance ratio
         acceptance_ratio = torch.squeeze(2 * (new_psi - cur_psi))
@@ -238,7 +244,7 @@ class MonteCarlo():
 
         The same process is followed for `cur_psi` and `new_psi`.
         """
-        cur_state = torch.where(accepted_bools, new_state, cur_state)
+        cur_state = torch.where(accepted_bools[:, None, None], new_state, cur_state)
         cur_psi = torch.where(accepted_bools, new_psi, cur_psi)
 
         # if we are storing our progress for analysis