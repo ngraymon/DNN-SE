aedb230a1f526e64c8d9b24bfe90dac9b3ef239c
Neil <ngraymon@uwaterloo.ca>
2021-12-08 09:35:14 -0500

    Fix - plotting
    small adjustments

--- plotting.py
+++ plotting.py
@@ -1,28 +1,48 @@
-import numpy as np
-from matplotlib import pyplot as plt
 
-import torch
+# system imports
 import os
 from os.path import join
+
+# third party imports
+import torch
+import numpy as np
+
+import matplotlib as mpl
+# `use_backend` needed if no x-server is available
+mpl.use(['pdf', 'Agg', 'svg'][1])
+from matplotlib import pyplot as plt
+
+# local imports
 from flags import flags
 
 
-def plot_helium(NN):
+def plot_helium(network):
+    """ Plot the helium wavefunction over a range of electron positions """
     plt.rcParams['text.usetex'] = True
     n_step_plot = flags.n_step_plot
     plot_path = flags.plot_path
+
     # fix the first electron for the Helium atom and vary the second
-    # (0.5,0,0)a_0
-    # (x,0,0)a_0, with x=linespace(-1,1,20)
+    # (0.5,0,0) a_0
+    # (x,0,0) a_0, with x=linespace(-1,1,20)
     # (0.5 cosθ,0.5 sinθ,0)a_0
-    linear = np.array([[[0.5, 0, 0], [x, 0, 0]] for x in np.linspace(-1, 1, n_step_plot)])
-    rotational = np.array([[[0.5, 0, 0], [0.5*np.cos(theta), 0.5*np.sin(theta), 0]] for theta in np.linspace(-180, 180, n_step_plot)])
-    phi_lin = NN.forward(torch.tensor(linear))
-    phi_rot = NN.forward(torch.tensor(rotational))
+
+    linear = np.array([
+        [[0.5, 0, 0], [x, 0, 0]]
+        for x in np.linspace(-1, 1, n_step_plot)
+    ])
+    rotational = np.array([
+        [[0.5, 0, 0], [0.5*np.cos(theta), 0.5*np.sin(theta), 0]]
+        for theta in np.linspace(-180, 180, n_step_plot)
+    ])
+
+    phi_lin = network.forward(torch.tensor(linear))
+    phi_rot = network.forward(torch.tensor(rotational))
 
     fig, ax = plt.subplots(2)
     fig.suptitle('Wave function for helium atom')
     fig.tight_layout()
+
     # Make your plot, set your axes labels
     ax[0].plot(torch.linspace(-1, 1, 10), phi_lin)
     ax[0].set_xlabel('x [Bohr]')
@@ -35,19 +55,30 @@ def plot_helium(NN):
     ax[1].set_xlabel(r'$\theta$')
     ax[1].set_ylabel(r' $\psi$')
     ax[1].set_yticklabels([])
+
     strFile = join(plot_path, 'Wavefunction.png')
+
+    # overwrite the previous file
     if os.path.isfile(strFile):
         os.remove(strFile)
+
     plt.savefig(strFile)
 
 
 def plot_loss(losstot):
+    """ Plot the loss per epoch """
+
     plt.rcParams['text.usetex'] = True
     plot_path = flags.plot_path
+
     plt.plot(losstot)
     plt.xlabel('Epoch')
     plt.ylabel('Local Energy (loss) [J]')
+
     strFile = join(plot_path, 'Local_Energy.png')
+
+    # overwrite the previous file
     if os.path.isfile(strFile):
         os.remove(strFile)
+
     plt.savefig(strFile)
